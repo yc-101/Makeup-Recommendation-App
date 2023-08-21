e19dbf0cd0b0dd5fd23373eee8a228806f1ea56a
Jennn <jenny091101@gmail.com>
2023-08-21 17:02:36 +0800

    start faceMesh(排版)

--- app/src/main/java/com/example/camera2/FaceMeshGraphic.kt
+++ app/src/main/java/com/example/camera2/FaceMeshGraphic.kt
@@ -19,6 +19,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
+import android.util.Log
 import com.google.mlkit.vision.common.PointF3D
 import com.example.camera2.GraphicOverlay
 import com.google.mlkit.vision.facemesh.FaceMesh
@@ -30,132 +31,161 @@ import com.google.mlkit.vision.facemesh.FaceMeshPoint
  * view.
  */
 class FaceMeshGraphic(overlay: GraphicOverlay?, private val faceMesh: FaceMesh) :
-  GraphicOverlay.Graphic(overlay) {
+	GraphicOverlay.Graphic(overlay) {
 
-  private val positionPaint: Paint
-  private val boxPaint: Paint
-  private val useCase: Int = USE_CASE_CONTOUR_ONLY // FaceMeshDetectorOptions.FACE_MESH
-  private var zMin: Float
-  private var zMax: Float
+	private val positionPaint: Paint
+	private val boxPaint: Paint
+	private val useCase: Int
+	private var zMin: Float
+	private var zMax: Float
 
-  @FaceMesh.ContourType
-  private val DISPLAY_CONTOURS =
-    intArrayOf(
-      FaceMesh.FACE_OVAL,
-      FaceMesh.LEFT_EYEBROW_TOP,
-//      FaceMesh.LEFT_EYEBROW_BOTTOM,
-//      FaceMesh.RIGHT_EYEBROW_TOP,
-      FaceMesh.RIGHT_EYEBROW_BOTTOM,
-//      FaceMesh.LEFT_EYE,
-//      FaceMesh.RIGHT_EYE,
-//      FaceMesh.UPPER_LIP_TOP,
-      FaceMesh.UPPER_LIP_BOTTOM,
-//      FaceMesh.LOWER_LIP_TOP,
-//      FaceMesh.LOWER_LIP_BOTTOM,
-      FaceMesh.NOSE_BRIDGE
-    )
+	@FaceMesh.ContourType
+	private val DISPLAY_CONTOURS =
+		intArrayOf(
+			FaceMesh.FACE_OVAL,
+			FaceMesh.LEFT_EYEBROW_TOP,
+			FaceMesh.LEFT_EYEBROW_BOTTOM,
+			FaceMesh.RIGHT_EYEBROW_TOP,
+			FaceMesh.RIGHT_EYEBROW_BOTTOM,
+			FaceMesh.LEFT_EYE,
+			FaceMesh.RIGHT_EYE,
+			FaceMesh.UPPER_LIP_TOP,
+			FaceMesh.UPPER_LIP_BOTTOM,
+			FaceMesh.LOWER_LIP_TOP,
+			FaceMesh.LOWER_LIP_BOTTOM,
+			FaceMesh.NOSE_BRIDGE
+		)
 
-  /** Draws the face annotations for position on the supplied canvas. */
-  override fun draw(canvas: Canvas) {
+	/** Draws the face annotations for position on the supplied canvas. */
+	override fun draw(canvas: Canvas) {
 
-    // Draws the bounding box.
-    val rect = RectF(faceMesh.boundingBox)
-    // If the image is flipped, the left will be translated to right, and the right to left.
-    val x0 = translateX(rect.left)
-    val x1 = translateX(rect.right)
-    rect.left = Math.min(x0, x1)
-    rect.right = Math.max(x0, x1)
-    rect.top = translateY(rect.top)
-    rect.bottom = translateY(rect.bottom)
-    canvas.drawRect(rect, boxPaint)
+		// Draws the bounding box. 臉的框框
+		val rect = RectF(faceMesh.boundingBox)
+		// If the image is flipped, the left will be translated to right, and the right to left.
+		val x0 = translateX(rect.left)
+		val x1 = translateX(rect.right)
+		rect.left = Math.min(x0, x1)
+		rect.right = Math.max(x0, x1)
+		rect.top = translateY(rect.top)
+		rect.bottom = translateY(rect.bottom)
+		canvas.drawRect(rect, boxPaint)
 
-    // Draw face mesh
-    val points =
-      if (useCase == USE_CASE_CONTOUR_ONLY) getContourPoints(faceMesh) else faceMesh.allPoints
-    val triangles = faceMesh.allTriangles
+		// Draw face mesh
+		val points =
+			if (useCase == USE_CASE_CONTOUR_ONLY) getContourPoints(faceMesh) else faceMesh.allPoints
+		val triangles = faceMesh.allTriangles
 
-    zMin = Float.MAX_VALUE
-    zMax = Float.MIN_VALUE
-    for (point in points) {
-      zMin = Math.min(zMin, point.position.z)
-      zMax = Math.max(zMax, point.position.z)
-    }
+		zMin = Float.MAX_VALUE
+		zMax = Float.MIN_VALUE
+		for (point in points) {
+			zMin = Math.min(zMin, point.position.z)
+			zMax = Math.max(zMax, point.position.z)
+		}
 
-    // Draw face mesh points
-    for (point in points) {
-      updatePaintColorByZValue(
-        positionPaint,
-        canvas,
-        /* visualizeZ= */true,
-        /* rescaleZForVisualization= */true,
-        point.position.z,
-        zMin,
-        zMax)
-      canvas.drawCircle(
-        translateX(point.position.x),
-        translateY(point.position.y),
-        FACE_POSITION_RADIUS,
-        positionPaint
-      )
-    }
+		// Draw face mesh points
+		for ((i, point) in points.withIndex()) {
+//      Log.d("test", "index: ${point.index}")
+			updatePaintColorByZValue(
+				positionPaint,
+				canvas,
+				/* visualizeZ= */true,
+				/* rescaleZForVisualization= */true,
+				point.position.z,
+				zMin,
+				zMax)
+			canvas.drawCircle(
+				translateX(point.position.x),
+				translateY(point.position.y),
+				FACE_POSITION_RADIUS,
+				positionPaint
+			)
+//      canvas.drawText("${i}",
+//        translateX(point.position.x)+2,
+//        translateY(point.position.y),
+//        positionPaint)
+		}
 
-    if (useCase == FaceMeshDetectorOptions.FACE_MESH) {
-      // Draw face mesh triangles
-      for (triangle in triangles) {
-        val point1 = triangle.allPoints[0].position
-        val point2 = triangle.allPoints[1].position
-        val point3 = triangle.allPoints[2].position
-        drawLine(canvas, point1, point2)
-        drawLine(canvas, point1, point3)
-        drawLine(canvas, point2, point3)
-      }
-    }
-  }
+		/** particular points to get face measure */
+		var testPoints = emptyList<Int>()
+		testPoints += listOf(
+			54, 284,  // 額頭
+			454, 234, // 顴骨
+			172, 397, // 下顎
+			5, 152    // 臉中心 底部 (用來算臉長)
+		)
+		for(testPoint in testPoints) {
+			canvas.drawCircle(
+				translateX(faceMesh.allPoints[testPoint].position.x),
+				translateY(faceMesh.allPoints[testPoint].position.y),
+				FACE_POSITION_RADIUS*3,
+				positionPaint
+			)
+			canvas.drawText("${faceMesh.allPoints[testPoint].index}",
+				translateX(faceMesh.allPoints[testPoint].position.x)+2,
+				translateY(faceMesh.allPoints[testPoint].position.y),
+				positionPaint)
+		}
 
-  private fun drawLine(canvas: Canvas, point1: PointF3D, point2: PointF3D) {
-    updatePaintColorByZValue(
-      positionPaint,
-      canvas,
-      /* visualizeZ= */true,
-      /* rescaleZForVisualization= */true,
-      (point1.z + point2.z) / 2,
-      zMin,
-      zMax)
-    canvas.drawLine(
-      translateX(point1.x),
-      translateY(point1.y),
-      translateX(point2.x),
-      translateY(point2.y),
-      positionPaint
-    )
-  }
+		if (useCase == FaceMeshDetectorOptions.FACE_MESH) {
+			// Draw face mesh triangles
+			for (triangle in triangles) {
+				val point1 = triangle.allPoints[0].position
+				val point2 = triangle.allPoints[1].position
+				val point3 = triangle.allPoints[2].position
+				drawLine(canvas, point1, point2)
+				drawLine(canvas, point1, point3)
+				drawLine(canvas, point2, point3)
+			}
+		}
+	}
 
-  private fun getContourPoints(faceMesh: FaceMesh): List<FaceMeshPoint> {
-    val contourPoints: MutableList<FaceMeshPoint> = ArrayList()
-    for (type in DISPLAY_CONTOURS) {
-      contourPoints.addAll(faceMesh.getPoints(type))
-    }
-    return contourPoints
-  }
+	private fun drawLine(canvas: Canvas, point1: PointF3D, point2: PointF3D) {
+		updatePaintColorByZValue(
+			positionPaint,
+			canvas,
+			/* visualizeZ= */true,
+			/* rescaleZForVisualization= */true,
+			(point1.z + point2.z) / 2,
+			zMin,
+			zMax)
+		canvas.drawLine(
+			translateX(point1.x),
+			translateY(point1.y),
+			translateX(point2.x),
+			translateY(point2.y),
+			positionPaint
+		)
+	}
 
-  companion object {
-    private const val USE_CASE_CONTOUR_ONLY = 999
-    private const val FACE_POSITION_RADIUS = 2.0f // 畫網格的點
-    private const val BOX_STROKE_WIDTH = 5.0f
-  }
+	private fun getContourPoints(faceMesh: FaceMesh): List<FaceMeshPoint> {
+		val contourPoints: MutableList<FaceMeshPoint> = ArrayList()
+		for (type in DISPLAY_CONTOURS) {
+			contourPoints.addAll(faceMesh.getPoints(type))
+		}
+		return contourPoints
+	}
 
-  init {
-    val selectedColor = Color.WHITE
-    positionPaint = Paint()
-    positionPaint.color = selectedColor
+	companion object {
+		private const val USE_CASE_CONTOUR_ONLY = 999
+		private const val FACE_POSITION_RADIUS = 2.0f // 畫網格的點
+		private const val BOX_STROKE_WIDTH = 5.0f
+	}
 
-    boxPaint = Paint()
-    boxPaint.color = selectedColor
-    boxPaint.style = Paint.Style.STROKE
-    boxPaint.strokeWidth = BOX_STROKE_WIDTH
+	init {
+		val selectedColor = Color.WHITE
+		positionPaint = Paint()
+		positionPaint.color = selectedColor
 
-    // useCase = PreferenceUtils.getFaceMeshUseCase(applicationContext)
-    zMin = java.lang.Float.MAX_VALUE
-    zMax = java.lang.Float.MIN_VALUE
-  }
+		boxPaint = Paint()
+		boxPaint.color = selectedColor
+		boxPaint.style = Paint.Style.STROKE
+		boxPaint.strokeWidth = BOX_STROKE_WIDTH
+
+		useCase =
+			FaceMeshDetectorOptions.FACE_MESH
+		// USE_CASE_CONTOUR_ONLY
+		// PreferenceUtils.getFaceMeshUseCase(applicationContext)
+		zMin = java.lang.Float.MAX_VALUE
+		zMax = java.lang.Float.MIN_VALUE
+	}
 }
